1da1db5531c2034db1fc7be969197c93fc7154b1
Windsvept <junsubjjang@gmail.com>
2018-11-01 16:33:34 -0700

    coin

--- index.js
+++ index.js
@@ -6,19 +6,32 @@ let renderer = new THREE.WebGLRenderer({ canvas: test });
 renderer.setSize(window.innerWidth/2, window.innerHeight/2);
 document.body.appendChild(renderer.domElement);
 
-let geometry = new THREE.BoxGeometry(5, 5, 5);
-let material = new THREE.MeshBasicMaterial( {color: 0xFFFFFF, wireframe: false} );
-let cube = new THREE.Mesh(geometry, material);
-let light = new THREE.DirectionalLight( 0xffffff, 1, 100 );
-light.position.set( 0, 1, 0 ); 			//default; light shining from top
-light.castShadow = true;            // default false
-scene.add( light );
-scene.add(cube);
-camera.position.z = 15;
+window.addEventListener('resize', () => {
+  let width = window.innerWidth;
+  let height = window.innerHeight;
+  renderer.setSize(width/2, height/2);
+  camera.aspect = width / height;
+  camera.updateProjectionMatrix();
+});
+
+// let geometry = new THREE.BoxGeometry(5, 5, 5);
+// let material = new THREE.MeshBasicMaterial( {color: 0xFFFFFF, wireframe: false} );
+// let cube = new THREE.Mesh(geometry, material);
+// scene.add(cube);
+
+var geometry = new THREE.TorusBufferGeometry( 10, 3, 16, 100 );
+var material = new THREE.MeshBasicMaterial( { color: 0xffff00 , wireframe: true} );
+var torus = new THREE.Mesh( geometry, material );
+scene.add( torus );
+camera.position.z = 100;
+// let light = new THREE.DirectionalLight( 0xffffff, 1, 100 );
+// light.position.set( 0, 1, 0 ); 			//default; light shining from top
+// light.castShadow = true;            // default false
+// scene.add( light );
 
 let update = () => {
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.05;
+  // torus.rotation.x += 0.01;
+  torus.rotation.y += 0.08;
 };
 
 let render = () => {
